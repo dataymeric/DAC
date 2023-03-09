2fda3bdb5d268abd06ddec47e1bac30914e7cb4a
dataymeric <13287345+dataymeric@users.noreply.github.com>
2023-03-09 18:02:41 +0100

    RI/TME1: initial commit

---
+++ RITAL/RI/TME1/tme1.py
@@ -0,0 +1,9 @@
+doc = [
+    "the new home has been saled on top forecasts",
+    "the home sales rise in july",
+    "there is an increase in home sales in july",
+    "july encounter a new home sales rise",
+]
+
+def traitement(document):
+    document.split(" ")
