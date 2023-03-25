ML/TME4: fix: w calculation refactor: spelling, var names

ML/TME4/src/tme4.py
@@ -4,7 +4,7 @@ from matplotlib import cm
 from mltools import plot_data, plot_frontiere, make_grid, gen_arti
 
 
-def reshape(w, x, y):
+def reshape(w, X, y):
     """Reshape les entrées.
 
     Returns
@@ -14,17 +14,17 @@ def reshape(w, x, y):
     y : array de taille (n, 1)
         Labels.
     """
-    return w.reshape(-1, 1), y.reshape(-1, 1)
+    return w.reshape(-1, 1), X.reshape(y.shape[0], -1), y.reshape(-1, 1)
 
 
-def perceptron_loss(w, x, y):
+def perceptron_loss(w, X, y):
     """Renvoie le coût d'un perceptron.
 
     Parameters
     ----------
     w : array de taille (d, 1)
         Vecteur poids.
-    x : array de taille (n, d)
+    X : array de taille (n, d)
         Données.
     y : array de taille (n, 1)
         Labels.
@@ -34,18 +34,18 @@ def perceptron_loss(w, x, y):
     z : array (n, 1)
         Coût du perceptron.
     """
-    w, y = reshape(w, x, y)
-    return np.maximum(0, -y * np.dot(x, w))
+    w, X, y = reshape(w, X, y)
+    return np.maximum(0, -y * np.dot(X, w))
 
 
-def perceptron_grad(w, x, y):
+def perceptron_grad(w, X, y):
     """Renvoie le gradient d'un perceptron.
 
     Parameters
     ----------
     w : array de taille (d, 1)
         Vecteur poids.
-    x : array de taille (n, d)
+    X : array de taille (n, d)
         Données.
     y : array de taille (n, 1)
         Labels.
@@ -55,24 +55,24 @@ def perceptron_grad(w, x, y):
     z : array (n, d)
         Gradient du perceptron.
     """
-    w, y = reshape(w, x, y)
-    return np.where(perceptron_loss(w, x, y) <= 0, 0, -y * x)
+    w, X, y = reshape(w, X, y)
+    return ((-y * np.dot(X, w)) > 0) * (-y * X)
 
 
-def hinge_loss(w, x, y, alpha, λ):
+def hinge_loss(w, X, y, alpha, lam):
     """Renvoie la hinge loss.
 
     Parameters
     ----------
     w : array de taille (d, 1)
         Vecteur poids.
-    x : array de taille (n, d)
+    X : array de taille (n, d)
         Données.
     y : array de taille (n, 1)
         Labels.
     alpha : int
         Hinge.
-    λ : int
+    lam : int
         Pénalisation.
 
     Returns
@@ -80,24 +80,24 @@ def hinge_loss(w, x, y, alpha, λ):
     z : array (n, 1)
         Hinge loss.
     """
-    w, y = reshape(w, x, y)
-    return np.maximum(0, alpha - y * np.dot(w, x)) + λ * np.linalg.norm(w) ** 2
+    w, X, y = reshape(w, X, y)
+    return np.maximum(0, alpha - y * np.dot(w, X)) + lam * np.linalg.norm(w) ** 2
 
 
-def hinge_loss_grad(w, x, y, alpha, λ):
+def hinge_loss_grad(w, X, y, alpha, lam):
     """Renvoie le gradient de la hinge loss.
 
     Parameters
     ----------
     w : array de taille (d, 1)
         Vecteur poids.
-    x : array de taille (n, d)
+    X : array de taille (n, d)
         Données.
     y : array de taille (n, 1)
         Labels.
     alpha : int
         Hinge.
-    λ : int
+    lam : int
         Pénalisation.
 
     Returns
@@ -105,23 +105,52 @@ def hinge_loss_grad(w, x, y, alpha, λ):
     z : array (n, 1)
         Hinge loss.
     """
-    w, y = reshape(w, x, y)
-    if -y * np.dot(w, x) + λ * np.linalg.norm(w) ** 2 < alpha:
-        return 2 * λ * w - y * x
-    return 0        
-
-def proj_biais(datax):
-    biais = np.ones((datax.shape[0], 1))
-    return np.hstack((biais, datax))
-
-
-def proj_poly(datax):
-    biais = np.ones((datax.shape[0], 1))
-    return np.hstack((biais, datax, datax * datax))
+    w, X, y = reshape(w, X, y)
+    z = y * np.dot(w, X)
+    if z >= alpha:
+        grad = 2 * lam * w
+    else:
+        grad = -y * X + 2 * lam * w
+    return grad
 
 
-def proj_gauss(datax, base, sigma):
-    return np.exp(-(np.linalg.norm(datax - base) ** 2) / (2 * sigma))
+def proj_biais(X):
+    """Introduit un biais."""
+    biais = np.ones((X.shape[0], 1))
+    return np.hstack((biais, X))
+
+
+def proj_poly(X):
+    """Projection polynomiale.
+
+    $X = (x_1, x_2, ..., x_d)$ =>
+    $(1, x_1, x_2, ..., x_d, x_1^2, x_1x_2, ..., x_d^2)$
+    """
+    n, d = X.shape
+    proj = np.ones((n, 1 + d + d * (d + 1) // 2))
+    for i in range(d):
+        proj[:, i + 1] = X[:, i]  
+        # Boucle imbriquée pour ajouter les colonnes quadratiques x_i^2 et x_i*x_j
+        for j in range(i, d):
+            col = 1 + d + i * d + j - i * (i + 1) // 2
+            if i == j:
+                proj[:, col] = X[:, i] ** 2
+            else:
+                proj[:, col] = X[:, i] * X[:, j]
+    return proj
+
+
+def proj_gauss(X, base, sigma):
+    """Projection gaussienne."""
+    n, d = X.shape
+    m, d = base.shape
+    proj = np.zeros((n, m))
+    for i in range(n):
+        for j in range(m):
+            proj[i, j] = np.exp(
+                -np.linalg.norm(X[i, :] - base[j, :]) ** 2 / (2 * sigma**2)
+            )
+    return proj
 
 
 class Lineaire(object):
@@ -140,14 +169,14 @@ class Lineaire(object):
         self.loss_g = loss_g
         self.projection = projection
 
-    def fit(self, datax, datay, batch_size=50, mode=None):
+    def fit(self, X, y, batch_size=50, mode=None, **kwargs):
         """Performe une descente de gradient.
 
         Parameters
         ----------
-        datax : array de taille (n, d)
+        X : array de taille (n, d)
             Données.
-        datay : array de taille (n, 1)
+        y : array de taille (n, 1)
             Labels.
         batch_size : int, default=50
             Nombre d'exemples par batchs pour la descente par mini-batch. Paramètre
@@ -164,73 +193,79 @@ class Lineaire(object):
         losses : array(self.max_iter)
             Historique des coûts au fur et à mesure de la descente de gradient.
         """
+        X_score = X.copy()
         if self.projection is not None:
-            datax = self.projection(datax)
-        self.w = np.random.randn(datax.shape[1], 1)
+            X = self.projection(X, **kwargs)
+        self.w = np.ones((X.shape[1], 1))
         weights = [self.w]
-        losses = []
+        losses = [self.loss(self.w, X, y).mean()]
+        scores = [self.score(X_score, y)]
+
         for _ in range(self.max_iter):
             if mode is None:
-                self.w -= self.eps * self.loss_g(self.w, datax, datay).mean()
+                grad = self.loss_g(self.w, X, y).mean(0).reshape(-1, 1)
+                self.w = self.w - self.eps * grad
             elif mode == "stochastique":
-                np.random.shuffle(datax)
-                np.random.shuffle(datay)
-                for rand in range(datax.shape[0]):
-                    self.w -= (
-                        self.eps * self.loss_g(self.w, datax[rand], datay[rand]).mean()
-                    )
+                np.random.shuffle(X)
+                np.random.shuffle(y)
+                for rand in range(X.shape[0]):
+                    grad = self.loss_g(self.w, X[rand], y[rand]).mean(0).reshape(-1, 1)
+                    self.w = self.w - self.eps * grad
             elif mode == "minibatch":
-                np.random.shuffle(datax)
-                np.random.shuffle(datay)
-                for i in range(0, datax.shape[0], batch_size):
-                    self.w -= (
-                        self.eps
-                        * self.loss_g(
-                            self.w, datax[i : i + batch_size], datay[i : i + batch_size]
-                        ).mean()
+                np.random.shuffle(X)
+                np.random.shuffle(y)
+                for i in range(0, X.shape[0], batch_size):
+                    grad = (
+                        self.loss_g(
+                            self.w, X[i : i + batch_size], y[i : i + batch_size]
+                        )
+                        .mean(0)
+                        .reshape(-1, 1)
                     )
+                    self.w = self.w - self.eps * grad
             else:
                 raise ValueError(
                     "mode must be one of {None, 'stochastique' or "
                     "'minibatch'}, got '%s' instead" % mode
                 )
-            losses.append(self.loss(self.w, datax, datay).mean())
             weights.append(self.w)
-        return self.w, np.array(weights), np.array(losses)
+            losses.append(self.loss(self.w, X, y).mean())
+            scores.append(self.score(X_score, y))
+        return self.w, np.array(weights), np.array(losses), np.array(scores)
 
-    def predict(self, datax):
+    def predict(self, X):
         """Infére le label des données."""
         if self.projection is not None:
-            datax = self.projection(datax)
-        return np.sign(np.dot(datax, self.w))
+            X = self.projection(X)
+        return np.sign(np.dot(X, self.w))
 
-    def score(self, datax, datay):
+    def score(self, X, y):
         """Calcule le pourcentage de bonne classification sur le jeu de données
         passé en paramètre.
         """
-        return np.mean(self.predict(datax) == datay)
+        return np.mean(self.predict(X) == y)
 
 
 def load_usps(fn):
     with open(fn, "r") as f:
         f.readline()
-        data = [[float(x) for x in l.split()] for l in f if len(l.split()) > 2]
+        data = [[float(X) for X in l.split()] for l in f if len(l.split()) > 2]
     tmp = np.array(data)
     return tmp[:, 1:], tmp[:, 0].astype(int)
 
 
-def get_usps(l, datax, datay):
+def get_usps(l, X, y):
     if type(l) != list:
-        resx = datax[datay == l, :]
-        resy = datay[datay == l]
+        resx = X[y == l, :]
+        resy = y[y == l]
         return resx, resy
-    tmp = list(zip(*[get_usps(i, datax, datay) for i in l]))
+    tmp = list(zip(*[get_usps(i, X, y) for i in l]))
     tmpx, tmpy = np.vstack(tmp[0]), np.hstack(tmp[1])
     return tmpx, tmpy
 
 
 def show_usps(data):
-    plt.imshow(data.reshape((16, 16)), interpolation="nearest", cmap="gray")
+    plt.imshow(data.reshape((16, 16)), interpolation="nearest")
 
 
 if __name__ == "__main__":